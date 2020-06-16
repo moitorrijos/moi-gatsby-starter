0b2ea6070f6b54d556680b57474ea3514c77e04d
moitorrijos <juanmtorrijos@gmail.com>
2020-06-15 19:22:13 -0500

    Add gatsby plugin robots txt. Gatsby plugin react svg.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+const siteUrl = 'http://site-url.com'
 module.exports = {
   siteMetadata: {
     title: `Tu Proyecto`,
@@ -25,6 +26,14 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: siteUrl,
+        sitemap: siteUrl + "/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -33,9 +42,17 @@ module.exports = {
         background_color: `#254395`,
         theme_color: `#254395`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/website-icon.png`,
       },
-    }
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /icons/,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
